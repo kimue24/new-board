03eb637265bd91e4b4e7256f4ca40284f6ad5d5b
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-10-14 09:27:28 +0900

    자바 업데이트
    20201014

---
+++ JavaStudy/src/OperatiorEx8.java
@@ -0,0 +1,14 @@
+
+public class OperatiorEx8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1_000_000;
+		int b = 2_000_000;
+		
+		long c= a*b;
+	
+		System.out.println(c);
+	}
+
+}
