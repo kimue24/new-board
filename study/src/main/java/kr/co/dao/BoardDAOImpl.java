ede9423a67bdf635de40fff723ba5bff7f9fe506
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-10-26 14:00:14 +0900

    게시판 검색기능추가

--- study/src/main/java/kr/co/dao/BoardDAOImpl.java
+++ study/src/main/java/kr/co/dao/BoardDAOImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import kr.co.vo.BoardVO;
 import kr.co.vo.Criteria;
+import kr.co.vo.SearchCriteria;
 
 @Repository
 public class BoardDAOImpl implements BoardDAO {
@@ -25,16 +26,16 @@ public class BoardDAOImpl implements BoardDAO {
 
 	// 게시물 목록 조회
 	@Override
-	public List<BoardVO> list(Criteria cri) throws Exception {
+	public List<BoardVO> list(SearchCriteria scri) throws Exception {
 	
-		return sqlSession.selectList("boardMapper.listPage", cri);
+		return sqlSession.selectList("boardMapper.listPage", scri);
 
 	}
 	
 	// 게시물 총 갯수
 	@Override
-	public int listCount() throws Exception {
-		return sqlSession.selectOne("boardMapper.listCount");
+	public int listCount(SearchCriteria scri) throws Exception {
+		return sqlSession.selectOne("boardMapper.listCount", scri);
 	}
 	
 	
