aeb30764acf2e6eb8d0a0f21634667f3c574f600
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-10-27 16:47:36 +0900

    댓글기능 업데이트
    댓댓

---
+++ study/src/main/java/kr/co/service/ReplyServicelmpl.java
@@ -0,0 +1,22 @@
+package kr.co.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.dao.ReplyDAO;
+import kr.co.vo.ReplyVO;
+
+@Service
+public class ReplyServicelmpl implements ReplyService{
+
+	@Inject
+	private ReplyDAO dao;
+	
+	@Override
+	public List<ReplyVO> readReply(int bno) throws Exception {
+		return dao.readReply(bno);
+	}
+}
