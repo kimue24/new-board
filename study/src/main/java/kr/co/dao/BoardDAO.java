ffbe0bba8a531b822a8d9b3fd6e5112b93c259b3
minho kim <54491294+kimue24@users.noreply.github.com>
2021-11-04 17:59:46 +0900

    update

--- study/src/main/java/kr/co/dao/BoardDAO.java
+++ study/src/main/java/kr/co/dao/BoardDAO.java
@@ -14,4 +14,11 @@ public interface BoardDAO {
 	
 	// 게시물 조회
 	public BoardVO read(int bno) throws Exception;
+	
+	// 게시물 수정
+	public void update(BoardVO boardVo) throws Exception;
+	
+	// 게시물 삭제
+	public void delete(int bno) throws Exception;
+	
 }
