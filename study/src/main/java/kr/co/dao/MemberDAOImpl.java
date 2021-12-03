5da44ad7f43f83202c5e690a9686d1815a7ac9ba
minho kim <54491294+kimue24@users.noreply.github.com>
2021-12-03 17:53:35 +0900

    회원가입진행완료

---
+++ study/src/main/java/kr/co/dao/MemberDAOImpl.java
@@ -0,0 +1,26 @@
+package kr.co.dao;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import kr.co.vo.MemberVO;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO {
+	
+	@Inject SqlSession sql;
+	
+	//회원가입
+	
+	@Override
+	public void register(MemberVO vo) throws Exception {
+		sql.insert("memberMapper.register", vo);
+	}
+	@Override
+	public MemberVO login(MemberVO vo) throws Exception {
+		return sql.selectOne("memberMapper.login", vo);
+	}
+
+}
